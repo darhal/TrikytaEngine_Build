1ab47106a468f58876b332002804a0d69e51230d
Omar <chidaomar46@gmail.com>
2018-09-24 19:37:29 +0200

    Modifed documentation

--- scripts/HowToLuaTrikyta.lua
+++ scripts/HowToLuaTrikyta.lua
@@ -20,35 +20,20 @@
 	*) isSpriteVisible(Sprite a) return a bool wether the sprite is visisble or not
 	
 #Animation Functions:
-	*) createAnimation(String SpritesPath, string ParserPath, int w, int h, int x, int y float UpdateRate) return animation
-	*) getSpritePosition(Animation a) return position of Animation
-	*) setSpritePosition(Animation a, x, y) return set pos of Animation to x, y
-	*) deleteSprite(Animation a) delete a Animation (DONT DELETE TWICE IT GONNA CRASH TRUST ME)
-	*) rotateSprite(Animation a, int Rot) Rotate a Animation
-	*) getSpriteRotation(Animation a) return the Animation rotation
-	*) flipSpriteHorizental(Animation a, bool isFlip) flip Animation horizentaly depending on the bool
-	*) flipSpriteVertical(Animation a, bool isFlip) flip Animation Vertical depending on the bool
-	*) setSpriteRotationCenter(a, int x, int y) Sets Animation rotation centre
-	*) getSpriteSize(Animation a) return int w, h Animation size
-	*) attachSpriteTo(Animation a, Animation b, int offsetX, int offsetY) attach Animation a to b in the given offset
-	*) setSpriteVisible(Animation a, bool visible) set Animation to visible or not (USE THIS INSTEAD OF DELETE)
-	*) isSpriteVisible(Animation a) return a bool wether the Animation is visisble or not
-	
-/************************ LEAGCY CODE : ********************************/
--THE FOLLOWING FUNCTION CHANGED SO PLEASE USE THE NEW SYNTAX:
-	*) CreateSprite(String path, int w, int h, int x, int y) return sprite
-	*) GetPos(Sprite a) return position of sprite
-	*) SetPos(a, x, y) return set pos of sprite to x, y
-	*) Sleep(int t) pause program for t milisecond (DELETED)
-	*) DelSprite(Sprite a) delete a sprite (DONT DELETE TWICE IT GONNA CRASH TRUST ME)
-In order to excute leagcy code on new builds Type this in config:
---LEAGCY CODE SUPPORT
-CreateSprite=createSprite
-SetPos=setSpritePosition
-GetPos=getSpritePosition
-DelSprite=deleteSprite
-/***********************************************************************/
-	
+	*) createAnimation(String AnimationsPath, string ParserPath, int w, int h, int x, int y float UpdateRate) return animation
+	*) getAnimationPosition(Animation a) return position of Animation
+	*) setAnimationPosition(Animation a, x, y) return set pos of Animation to x, y
+	*) deleteAnimation(Animation a) delete a Animation (DONT DELETE TWICE IT GONNA CRASH TRUST ME)
+	*) rotateAnimation(Animation a, int Rot) Rotate a Animation
+	*) getAnimationRotation(Animation a) return the Animation rotation
+	*) flipAnimationHorizental(Animation a, bool isFlip) flip Animation horizentaly depending on the bool
+	*) flipAnimationVertical(Animation a, bool isFlip) flip Animation Vertical depending on the bool
+	*) setAnimationRotationCenter(a, int x, int y) Sets Animation rotation centre
+	*) getAnimationSize(Animation a) return int w, h Animation size
+	*) attachAnimationTo(Animation a, Animation b, int offsetX, int offsetY) attach Animation a to b in the given offset
+	*) setAnimationVisible(Animation a, bool visible) set Animation to visible or not (USE THIS INSTEAD OF DELETE)
+	*) isAnimationVisible(Animation a) return a bool wether the Animation is visisble or not
+
 --EVENTS:
 	AddEventHandler("OnKeyboardInput", OnKeyboardInput)
 	AddEventHandler("OnMouseClick", OnMouseClick)
